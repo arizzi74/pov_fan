d624778dcf39444687d64f8742546fe59ef4badf
Antonio Rizzi <43698382+arizzi74@users.noreply.github.com>
2025-07-20 09:37:33 +0200

    Create demo_pov.py

---
+++ 2025_update/demo_pov.py
@@ -0,0 +1,11 @@
+from pov import POV
+
+POV(
+    screens = [
+        # Each line is a screen. Do not exceed 18 characters per line
+        # use only chars prsente in the map at top of file pov.py
+        'These fans ar cool',
+        'and make you cool!',
+           ],
+    name = 'my_pov_sketch'
+)
